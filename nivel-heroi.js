b5c85c57ac3abf00a98539efd44bf6a0d7eb84c6
larissa-albuquerque <l.fralbuquerque@gmail.com>
2025-10-02 16:26:11 -0300

    Desafio Nível do Herói

--- nivel-heroi.js
+++ nivel-heroi.js
@@ -1,24 +1,24 @@
 let nomeHeroi = "Pralim "
-let xpHeroi = 10001
+let xpHeroi = 8995
 let nivelHeroi
 
     if (xpHeroi<=1000)
         {
             nivelHeroi = "Ferro"
         }
-        else if (xpHeroi>=1001 && xpHeroi<2000){
+        else if (xpHeroi>=1001 && xpHeroi<=2000){
             nivelHeroi = "Bronze"
         }
-        else if (xpHeroi>=2001 && xpHeroi<5000){
+        else if (xpHeroi>=2001 && xpHeroi<=5000){
             nivelHeroi = "Prata"
         }
-        else if (xpHeroi>=5001 && xpHeroi<6000){
+        else if (xpHeroi>=5001 && xpHeroi<=6000){
             nivelHeroi = "Ouro"
         }
-        else if (xpHeroi>=6001 && xpHeroi<8000){
+        else if (xpHeroi>=6001 && xpHeroi<=8000){
             nivelHeroi = "Platina"
         }
-        else if (xpHeroi>=8001 && xpHeroi<9000){
+        else if (xpHeroi>=8001 && xpHeroi<=9000){
             nivelHeroi = "Ascendente"
         }
         else if (xpHeroi>=9001 && xpHeroi<10000){
